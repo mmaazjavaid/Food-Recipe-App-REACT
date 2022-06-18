14927889fca653628f0aa052d77b97fa5dd24b3f
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-18 23:56:45 +0500

    working on category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa'
-import {GiNoddles,GiChopSticks} from 'react-icons/Gi'
+import {GiNoodles, GiChopsticks} from 'react-icons/gi'
 import {Pizza} from '@styled-icons/fluentui-system-filled/FoodPizza';
-
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom';
 function Category() {
   return (
     <>
+    <List>
+      <NavLink to={'/cuisine/Italian'}>
+        <FaPizzaSlice/>
+        <h4>
+          Italian
+        </h4>
+      </NavLink>
+      <NavLink  to={'/cuisine/Italian'}>
+        <FaHamburger/>
+        <h4>
+          American
+        </h4>
+      </NavLink>
+      <NavLink to={'/cuisine/Italian'}>
+        <GiNoodles/>
+        <h4>
+          Thai
+        </h4>
+      </NavLink>
+      <NavLink to={'/cuisine/Italian'}>
+        <GiChopsticks/>
+        <h4>
+          Italian
+        </h4>
+      </NavLink>
+    </List>
     </>
   )
 }
-
+const List= styled.div `
+display:flex;
+justify-content:center;
+margin:2rem 0rem;
+`;
 export default Category
